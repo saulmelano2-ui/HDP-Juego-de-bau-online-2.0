ce1e723027456920f6d253a7b7b29cf0c16d4c66
saulmelano2-ui <saulmelano2@gmail.com>
2026-03-12 16:36:30 -0300

    Update server.js

--- server.js
+++ server.js
@@ -871,6 +871,7 @@ io.on('connection', (socket) => {
 });
 
 const PORT = process.env.PORT || 3000;
+
 server.listen(PORT, () => {
-  console.log(`🃏 HDP Cartas corriendo en http://localhost:${PORT}`);
+  console.log("Server running on port " + PORT);
 });
